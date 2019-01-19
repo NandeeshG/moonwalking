4272fc7f4b86e13e49709c30b9135726e9dc5443
NandeeshG <nandeeshgupta@gmail.com>
2019-01-19 20:19:13 +0530

    lab question 2 done

---
+++ compLab/geometric.c
@@ -0,0 +1,14 @@
+//input a,n,r and output sum of corresponding Geometric Series
+// a + a*r + a*r*r + a*r*r*r ...
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a, n, r;
+    printf("Input a, n, r -> ");
+    scanf("%d %d %d", &a, &n, &r);
+    int sum = a * (pow(r, n) - 1) / (r - 1);
+    printf("Geometric series sum = %d", sum);
+    return 0;
+}
